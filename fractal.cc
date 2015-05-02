12825b413560b3cdb8966af09bfaa5388f5b7d30
JuanI <JuanI@Juani-Lap>
2015-05-02 17:33:12 -0500

    new commit

--- fractal.cc
+++ fractal.cc
@@ -182,6 +182,7 @@ double wallTime() {
 
 int main(int argc, char* argv[]) {
     printf("Calculating. Please wait....\n");
+    printf("HOLA MUNDO");
     // tracking  wall time
     double startwtime = 0.0;
     double endwtime;
